e2c9a911a5387af78779bee5014b5905ee206a26
Reywam <reywam@mail.ru>
2019-08-26 14:02:17 +0300

    Добавлена простая проверка запрашиваемой даты на соответствие регулярному выражению

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/weather")
 public class MainController {
+
+    private Validator validator = new Validator();
+
     @GetMapping("/{date}")
     public String getWeatherDataByDate(@PathVariable(value="date") String date)
     {
+        if(!validator.isValidDate(date))
+        {
+            return "Date is not valid";
+        }
         return "Return all weather data by date:".concat(date);
     }
 
---
+++ src/main/java/app/Validator.java
@@ -0,0 +1,22 @@
+package app;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    private final String DATE_PATTERN = "\\d{2}.\\d{2}.\\d{4}";
+    private Pattern pattern = Pattern.compile(DATE_PATTERN);
+
+    public boolean isValidDate(String date)
+    {
+        Matcher matcher = pattern.matcher(date);
+        boolean isValidDatePattern = matcher.matches();
+        return isValidDatePattern;
+        // TODO Добавить проверку даты на корректность
+    }
+
+}
