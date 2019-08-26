22cd04593e94c079dc87c2092020d383587e3adf
Reywam <reywam@mail.ru>
2019-08-26 14:15:55 +0300

    Добавлен метод, который раз в 3 секунды делает запрос к openweathermap за данными о текущей погоде в Лондоне и выводит их в консоль

--- src/main/java/app/MainController.java
+++ src/main/java/app/MainController.java
@@ -1,15 +1,20 @@
 package app;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import static app.Constants.API_KEY;
+import static app.Constants.API_URL;
 
 @RestController
 @RequestMapping("/weather")
 public class MainController {
-
     private Validator validator = new Validator();
+    private WebClient.Builder builder = WebClient.builder();
 
     @GetMapping("/{date}")
     public String getWeatherDataByDate(@PathVariable(value="date") String date)
@@ -19,6 +24,19 @@ public class MainController {
             return "Date is not valid";
         }
         return "Return all weather data by date:".concat(date);
+    }
+
+    @Scheduled(fixedRate = 3000)
+    public void getWeatherData()
+    {
+        String uri = API_URL.concat(API_KEY);
+        String data = builder.build()
+                .get()
+                .uri(uri)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        System.out.println(data);
     }
 
 
